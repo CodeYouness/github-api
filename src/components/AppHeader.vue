e9adbc66fb857679f49577b98ea927f10b7a5df4
CodeYouness <ylijassi@gmail.com>
2024-10-08 16:32:40 +0200

    added header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+    data() {
+        return {
+        }
+    },
+}
+</script>
+
+<template>
+
+    <nav class="navbar bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand position-absolute">GitHub API</a>
+            <form class="d-flex me-auto ms-auto" role="search">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </div>
+    </nav>
+
+</template>
+
+<style scoped></style>

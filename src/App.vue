64696723791a1ad4906c1ece1b941cf99ebc2dda
CodeYouness <ylijassi@gmail.com>
2024-10-08 16:19:33 +0200

    added bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,18 @@
 <script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
 
+export default {
+  data() {
+    return {
+    }
+  },
+
+  components: {
+    AppHeader,
+    AppMain
+  }
+}
 </script>
 
 <template>

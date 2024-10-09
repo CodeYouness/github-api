f862070535f411b74d35f2ad2534482f1de498ba
CodeYouness <ylijassi@gmail.com>
2024-10-09 10:43:42 +0200

    added api call in appmain

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -4,6 +4,20 @@ export default {
         return {
         }
     },
+    methods: {
+        getCardList() {
+            axios.get('https://api.github.com/search/repositories?q=' + this.store.searchedType)
+                .then((response) => {
+                    // handle success
+                    console.log(response.data.data);
+                    this.store.cardList = response.data.data
+                })
+                .catch(function (error) {
+                    // handle error
+                    console.log(error);
+                })
+        },
+    }
 }
 </script>
 

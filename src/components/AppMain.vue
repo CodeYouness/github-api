1e0f32561c13b653bfc3e3b1a6f977cd73fcb1bb
CodeYouness <ylijassi@gmail.com>
2024-10-09 11:37:47 +0200

    added single components for card of repo

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,13 +1,20 @@
 <script>
+import AppMainCard from "./AppMainCard.vue";
+
 export default {
     data() {
         return {
+            AppMainCard
         }
     },
 }
 </script>
 
 <template>
+
+    <section class="container overflow-y-scroll">
+        <AppMainCard />
+    </section>
 
 </template>
 
---
+++ src/components/AppMainCard.vue
@@ -0,0 +1,25 @@
+<script>
+import { store } from "../store";
+import MainSingleCard from "./MainSingleCard.vue";
+
+export default {
+    data() {
+        return {
+            store
+        }
+    },
+    components: {
+        MainSingleCard
+    }
+
+}
+</script>
+
+<template>
+    <div class="row">
+        <MainSingleCard />
+    </div>
+
+</template>
+
+<style scoped></style>
